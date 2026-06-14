82c0f60d0d53dcc8d439dd1cb50a34f5e620250d
logeshm2006 <logeshmadhavan2006@gmail.com>
2026-06-14 13:48:11 +0530

    fix: model import crash

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Grievance = require('./models/Grievance');
+const Grievance = require('./models/Grievance.js');
 
 const app = express();
 
